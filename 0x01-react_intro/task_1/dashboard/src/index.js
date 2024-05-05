cea817031b8b27adc7d6efd24a70688781ea26f6
Jaybabson007 <yahayajoel2@gmail.com>
2024-05-05 04:59:51 +0100

    render bug fixed

--- 0x01-react_intro/task_1/dashboard/src/index.js
+++ 0x01-react_intro/task_1/dashboard/src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
+import ReactDOM from 'react-dom';
+//import './index.css';
 import App from './App';
 import Notifications from './Notifications';
 import reportWebVitals from './reportWebVitals';
